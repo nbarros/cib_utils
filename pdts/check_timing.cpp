cc6235cad64a25f35d1f7861e1c56243c90082c7
Nuno Barros <barros@lip.pt>
2024-04-09 15:17:39 +0100

    Adding the possibility to issue a PDTS reset

--- pdts/check_timing.cpp
+++ pdts/check_timing.cpp
@@ -8,7 +8,7 @@
 #include <thread>
 #include <chrono>
 
-// not used
+// reg 0 ised for reset
 #define GPIO1_MEM_LOW   0x00A0010000
 #define GPIO1_MEM_HIGH  0x00A001FFFF
 
@@ -45,6 +45,13 @@ typedef struct pdts_mon_t {
   uint32_t get_u32() {return *(reinterpret_cast<uint32_t*>(this));}
 
 } pdts_mon_t;
+
+typedef struct pdts_ctl_t {
+  uint32_t padding : 31;
+  uint32_t reset : 1;
+  uint32_t get_u32() {return *(reinterpret_cast<uint32_t*>(this));}
+
+} pdts_ctl_t;
 
 void test_structures()
 {
@@ -90,9 +97,21 @@ int main(int argc, char const *argv[])
     return 255;
   }
 
+  printf(">> Mapping control register\n");
+  uintptr_t vmem_ctl = cib::util::map_phys_mem(memfd,GPIO1_MEM_LOW,GPIO1_MEM_HIGH);
+  printf(">>> Got 0x%" PRIXPTR "\n",vmem_ctl);
+  if (vmem_ctl == 0x0)
+  {
+    cib::util::unmap_mem(cib::util::cast_to_void(vmem_ts),(GPIO3_MEM_HIGH-GPIO3_MEM_LOW));
+    cib::util::unmap_mem(cib::util::cast_to_void(vmem_mon),(GPIO2_MEM_HIGH-GPIO2_MEM_LOW));
+    return 255;
+  }
+
   //
   pdts_ts_t ts;
   pdts_mon_t mon;
+  pdts_ctl_t ctl;
+  bool first = true;
   while (!g_stop.load())
   {
     ts.low = cib::util::reg_read((vmem_ts+CH0_OFFSET));
@@ -102,7 +121,7 @@ int main(int argc, char const *argv[])
     *reinterpret_cast<uint32_t*>(&mon) = cib::util::reg_read((vmem_mon+CH0_OFFSET));
 
 
-    printf("ts : %" PRIu64 "   stat: %" PRIu32 "\n", ts.get_timestamp(), mon.get_u32());
+    printf("ts : %" PRIu64 "   stat: %" PRIX32 "\n", ts.get_timestamp(), mon.get_u32());
     printf("stat %X ctl_a %X ctl_d %X cdr_los %X sync %X sync_stb %X rdy %X sfp_tx_fault %X, sfp_los %X \n",
            mon.stat,
            mon.ctrl_a,
@@ -114,6 +133,21 @@ int main(int argc, char const *argv[])
            mon.sfp_tx_fault,
            mon.sfp_los);
 
+    if (first)
+    {
+      printf("> Applying reset \n");
+      *reinterpret_cast<uint32_t*>(&ctl) = cib::util::reg_read((vmem_ctl+CH0_OFFSET));
+      printf(">> Before : %" PRIX32 "\n",ctl.get_u32());
+      ctl.reset = 1;
+      cib::util::reg_write_mask(vmem_ctl,ctl.get_u32(),(1<<31));
+      std::this_thread::sleep_for(std::chrono::microseconds(2));
+      *reinterpret_cast<uint32_t*>(&ctl) = cib::util::reg_read((vmem_ctl+CH0_OFFSET));
+      printf(">> During : %" PRIX32 "\n",ctl.get_u32());
+      ctl.reset = 0;
+      cib::util::reg_write_mask(vmem_ctl,ctl.get_u32(),(1<<31));
+      *reinterpret_cast<uint32_t*>(&ctl) = cib::util::reg_read((vmem_ctl+CH0_OFFSET));
+      printf(">> After : %" PRIX32 "\n",ctl.get_u32());
+    }
    std::this_thread::sleep_for(std::chrono::seconds(1));
   }
 
