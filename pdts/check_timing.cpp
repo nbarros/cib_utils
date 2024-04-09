e0e80b9843ff66880f7fa8e68aedbcfa0e03abce
Nuno Barros <barros@lip.pt>
2024-04-09 14:10:36 +0100

    Added pdts monitor to test timing system

--- pdts/check_timing.cpp
+++ pdts/check_timing.cpp
@@ -2,14 +2,17 @@
 #include <cstdint>
 #include <cinttypes>
 
-#define GPIO1_MEM_LOW   0x00000A000000
-#define GPIO1_MEM_HIGH  0x00000A00FFFF
+// not used
+#define GPIO1_MEM_LOW   0x00A0010000
+#define GPIO1_MEM_HIGH  0x00A001FFFF
 
-#define GPIO2_MEM_LOW   0x00000A100000
-#define GPIO2_MEM_HIGH  0x00000A10FFFF
+// pdts debug (both registers look the same)
+#define GPIO2_MEM_LOW   0x00A0020000
+#define GPIO2_MEM_HIGH  0x00A002FFFF
 
-#define GPIO3_MEM_LOW   0x00000A200000
-#define GPIO3_MEM_HIGH  0x00000A20FFFF
+// holds the timestamp
+#define GPIO3_MEM_LOW   0x00A0030000
+#define GPIO3_MEM_HIGH  0x00A003FFFF
 
 #define CH0_OFFSET  0x0
 #define CH1_OFFSET  0x8
@@ -23,10 +26,15 @@ typedef struct ts_t {
 
 // struct of GPIO2
 typedef struct pdts_mon_t {
-  uint32_t ctrl : 6;
-  uint32_t reg2 : 11;
-  uint32_t reg3 : 10;
-  uint32_t reg4 : 5;
+  uint32_t stat : 4;
+  uint32_t ctrl_a : 7;
+  uint32_t ctrl_d : 8;
+  uint32_t cdr_los: 1;
+  uint32_t sync : 8;
+  uint32_t sync_stb :1;
+  uint32_t rdy : 1;
+  uint32_t sfp_tx_fault : 1;
+  uint32_t sfp_los : 1;
   uint32_t get_u32() {return *(reinterpret_cast<uint32_t*>(this));}
 
 } pdts_mon_t;
@@ -37,13 +45,16 @@ int main(int argc, char const *argv[])
 
   ts_t t;
   t.low = 0xFFFF;
-  t.high= 0xAAAAAAAAAA;
+  t.high= static_cast<uint32_t>(0xAAAAAAAAAA);
 
   printf("timestamp : 0x%" PRIX64 "\n",t.get_timestamp());
   pdts_mon_t mon;
-  *reinterpret_cast<uint32_t*>(&mon) = 0xAAAAAAAAAA;
+  *reinterpret_cast<uint32_t*>(&mon) = static_cast<uint32_t>(0xAAAAAAAAAA);
   printf("Size %lu value : 0x%" PRIX32 "\n",sizeof(mon),mon.get_u32());
 
+
+  // first map the memory
+  // we do need that
 
   return 0;
 }
