768a88a22f606e6087415057d4e5624679dfb44c
Nuno Barros <barros@lip.pt>
2024-04-17 16:41:57 +0100

    Updating the code to apply a reset prior to reading.

--- axi_fifo/read_axi_fifo.cpp
+++ axi_fifo/read_axi_fifo.cpp
@@ -39,7 +39,7 @@
 //#define printf(fmt, ...) printf("DEBUG %s:%d(): " fmt,  __func__, __LINE__, ##__VA_ARGS__)
 
 struct thread_data {
-    int rc;
+  int rc;
 };
 
 //pthread_t write_to_fifo_thread;
@@ -64,219 +64,224 @@ static void quit(void);
  *----------------------------------------------------------------------------*/
 int main(int argc, char **argv)
 {
-    process_options(argc, argv);
+  process_options(argc, argv);
 
-    int rc;
+  int rc;
 
-    // Listen to ctrl+c and assert
-    signal(SIGINT, signal_handler);
-    signal(SIGTERM, signal_handler);
-    signal(SIGQUIT, signal_handler);
+  // Listen to ctrl+c and assert
+  signal(SIGINT, signal_handler);
+  signal(SIGTERM, signal_handler);
+  signal(SIGQUIT, signal_handler);
 
-    /*************/
-    /* open FIFO */
-    /*************/
-    readFifoFd = open(_opt_dev_rx, O_RDONLY | O_NONBLOCK);
-    if (readFifoFd < 0) {
-        printf("Open read failed with error: %s\n", strerror(errno));
-        return -1;
-    }
+  /*************/
+  /* open FIFO */
+  /*************/
+  readFifoFd = open(_opt_dev_rx, O_RDONLY | O_NONBLOCK);
+  if (readFifoFd < 0) {
+    printf("Open read failed with error: %s\n", strerror(errno));
+    return -1;
+  }
 
-    /*****************************/
-    /* initialize the fifo core  */
-    /*****************************/
-    printf("Reset\n");
-    rc = ioctl(readFifoFd, AXIS_FIFO_RESET_IP);
-    if (rc) {
-        perror("ioctl");
-        return -1;
-    }
-//    rc = ioctl(writeFifoFd, AXIS_FIFO_RESET_IP);
-//    if (rc) {
-//        perror("ioctl");
-//        return -1;
-//    }
+  /*****************************/
+  /* initialize the fifo core  */
+  /*****************************/
 
-    /*****************/
-    /* start threads */
-    /*****************/
+  //NFB: For some reason, the reset does not work, causing an
+  // The IOCTLS were only implemented in the version 4.2 of the module
 
-    /* start thread listening for ethernet packets */
-//    rc = pthread_create(&write_to_fifo_thread, NULL, write_to_fifo_thread_fn,
-//            (void *)NULL);
+  printf("Reset\n");
+  rc = ioctl(readFifoFd, AXIS_FIFO_RESET_IP);
+  if (rc) {
+    perror("ioctl");
+    return -1;
+  }
 
-    /* start thread listening for fifo receive packets */
-    printf("Thread\n");
+  //    rc = ioctl(writeFifoFd, AXIS_FIFO_RESET_IP);
+  //    if (rc) {
+  //        perror("ioctl");
+  //        return -1;
+  //    }
 
-    rc = pthread_create(&read_from_fifo_thread, NULL, read_from_fifo_thread_fn,
-            (void *)NULL);
+  /*****************/
+  /* start threads */
+  /*****************/
 
-    /* perform noops */
-    while (running) {
-       sleep(1);
-    }
+  /* start thread listening for ethernet packets */
+  //    rc = pthread_create(&write_to_fifo_thread, NULL, write_to_fifo_thread_fn,
+  //            (void *)NULL);
 
-    printf("SHUTTING DOWN\n");
-//    pthread_join(write_to_fifo_thread, NULL);
-    pthread_join(read_from_fifo_thread, NULL);
-//    close(writeFifoFd);
-    close(readFifoFd);
-    return rc;
+  /* start thread listening for fifo receive packets */
+  printf("Thread\n");
+
+  rc = pthread_create(&read_from_fifo_thread, NULL, read_from_fifo_thread_fn,
+                      (void *)NULL);
+
+  /* perform noops */
+  while (running) {
+    sleep(1);
+  }
+
+  printf("SHUTTING DOWN\n");
+  //    pthread_join(write_to_fifo_thread, NULL);
+  pthread_join(read_from_fifo_thread, NULL);
+  //    close(writeFifoFd);
+  close(readFifoFd);
+  return rc;
 }
 
 static void quit(void)
 {
-    running = false;
+  running = false;
 }
 
 static void *write_to_fifo_thread_fn(void *data)
 {
-    int rc;
-    int packets_tx;
-    ssize_t bytesFifo;
-    char buf[MAX_BUF_SIZE_BYTES];
-    uint32_t vacancy;
+  int rc;
+  int packets_tx;
+  ssize_t bytesFifo;
+  char buf[MAX_BUF_SIZE_BYTES];
+  uint32_t vacancy;
 
-    /* shup up compiler */
-    (void)data;
+  /* shup up compiler */
+  (void)data;
 
-    packets_tx = 0;
+  packets_tx = 0;
 
-    while (running) {
-        do {
-            rc = ioctl(writeFifoFd, AXIS_FIFO_GET_TX_VACANCY, &vacancy);
-            if (rc) {
-                perror("ioctl");
-                return (void *)0;
-            }
-            if (vacancy < (uint32_t)MAX_BUF_SIZE_BYTES)
-                usleep(100);
-        } while (vacancy < (uint32_t)MAX_BUF_SIZE_BYTES);
+  while (running) {
+    do {
+      rc = ioctl(writeFifoFd, AXIS_FIFO_GET_TX_VACANCY, &vacancy);
+      if (rc) {
+        perror("ioctl");
+        return (void *)0;
+      }
+      if (vacancy < (uint32_t)MAX_BUF_SIZE_BYTES)
+        usleep(100);
+    } while (vacancy < (uint32_t)MAX_BUF_SIZE_BYTES);
 
-        printf("Send a message to %s : ",_opt_dev_tx);
-        scanf("%s",&buf[0]);
-        printf("Sending %s\n\r",buf);
+    printf("Send a message to %s : ",_opt_dev_tx);
+    scanf("%s",&buf[0]);
+    printf("Sending %s\n\r",buf);
 
-        bytesFifo = write(writeFifoFd, buf, strlen(buf));
-        if (bytesFifo > 0) {
-            printf("bytes to fifo %ld\n",bytesFifo);
-            packets_tx++;
-        } else {
-            perror("write");
-            quit();
-        }
+    bytesFifo = write(writeFifoFd, buf, strlen(buf));
+    if (bytesFifo > 0) {
+      printf("bytes to fifo %ld\n",bytesFifo);
+      packets_tx++;
+    } else {
+      perror("write");
+      quit();
     }
+  }
 
-    return (void *)0;
+  return (void *)0;
 }
 
 static void *read_from_fifo_thread_fn(void *data)
 {
-    ssize_t bytesFifo;
-    int packets_rx;
-    uint8_t buf[MAX_BUF_SIZE_BYTES];
-    uint64_t ts;
-    /* shup up compiler */
-    (void)data;
+  ssize_t bytesFifo;
+  int packets_rx;
+  uint8_t buf[MAX_BUF_SIZE_BYTES];
+  uint64_t ts;
+  /* shup up compiler */
+  (void)data;
 
-    packets_rx = 0;
+  packets_rx = 0;
 
-    printf("Looping\n");
+  printf("Looping\n");
 
-    while (running)
-    {
-        bytesFifo = read(readFifoFd, buf, MAX_BUF_SIZE_BYTES);
-        if (bytesFifo > 0) {
-            printf("bytes from fifo %ld\n",bytesFifo);
-            printf("Read : %s\n\r",buf);
-            ts = *reinterpret_cast<uint64_t*>(&buf);
-            printf("Read : %" PRIu64 "\n",ts);
+  while (running)
+  {
+    bytesFifo = read(readFifoFd, buf, MAX_BUF_SIZE_BYTES);
+    if (bytesFifo > 0) {
+      printf("bytes from fifo %ld\n",bytesFifo);
+      printf("Read : %s\n\r",buf);
+      ts = *reinterpret_cast<uint64_t*>(&buf);
+      printf("Read : %" PRIu64 "\n",ts);
 
-            packets_rx++;
-        }
+      packets_rx++;
     }
-    printf("Out of loop\n");
+  }
+  printf("Out of loop\n");
 
 
-    return (void *)0;
+  return (void *)0;
 }
 
 static void signal_handler(int signal)
 {
-    switch (signal) {
-        case SIGINT:
-        case SIGTERM:
-        case SIGQUIT:
-            running = false;
-            break;
+  switch (signal) {
+    case SIGINT:
+    case SIGTERM:
+    case SIGQUIT:
+      running = false;
+      break;
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 }
 
 static void display_help(char * progName)
 {
-    printf("Usage : %s [OPTIONS]\n"
-           "\n"
-           "  -h, --help     Print this menu\n"
-           "  -t, --devTx    Device to use ... /dev/axis_fifo_0x43c10000\n"
-           "  -r, --devRx    Device to use ... /dev/axis_fifo_0x43c10000\n"
-           ,
-           progName
-          );
+  printf("Usage : %s [OPTIONS]\n"
+      "\n"
+      "  -h, --help     Print this menu\n"
+      "  -t, --devTx    Device to use ... /dev/axis_fifo_0x43c10000\n"
+      "  -r, --devRx    Device to use ... /dev/axis_fifo_0x43c10000\n"
+      ,
+      progName
+  );
 }
 
 static void print_opts()
 {
-    printf("Options : \n"
-            "DevTX          : %s\n"
-            "DevRx          : %s\n"
-           ,
-           _opt_dev_tx,
-           _opt_dev_rx
-          );
+  printf("Options : \n"
+      "DevTX          : %s\n"
+      "DevRx          : %s\n"
+      ,
+      _opt_dev_tx,
+      _opt_dev_rx
+  );
 }
 
 static int process_options(int argc, char * argv[])
 {
-        strcpy(_opt_dev_tx,DEF_DEV_TX);
-        strcpy(_opt_dev_rx,DEF_DEV_RX);
+  strcpy(_opt_dev_tx,DEF_DEV_TX);
+  strcpy(_opt_dev_rx,DEF_DEV_RX);
 
-        for (;;) {
-            int option_index = 0;
-            static const char *short_options = "hr:t:";
-            static const struct option long_options[] = {
-                    {"help", no_argument, 0, 'h'},
-                    {"devRx", required_argument, 0, 'r'},
-                    {"devTx", required_argument, 0, 't'},
-                    {0,0,0,0},
-                    };
+  for (;;) {
+    int option_index = 0;
+    static const char *short_options = "hr:t:";
+    static const struct option long_options[] = {
+        {"help", no_argument, 0, 'h'},
+        {"devRx", required_argument, 0, 'r'},
+        {"devTx", required_argument, 0, 't'},
+        {0,0,0,0},
+    };
 
-            int c = getopt_long(argc, argv, short_options,
-            long_options, &option_index);
+    int c = getopt_long(argc, argv, short_options,
+                        long_options, &option_index);
 
-            if (c == EOF) {
-            break;
-            }
+    if (c == EOF) {
+      break;
+    }
 
-            switch (c) {
-                case 't':
-                    strcpy(_opt_dev_tx, optarg);
-                    break;
+    switch (c) {
+      case 't':
+        strcpy(_opt_dev_tx, optarg);
+        break;
 
-                case 'r':
-                    strcpy(_opt_dev_rx, optarg);
-                    break;
+      case 'r':
+        strcpy(_opt_dev_rx, optarg);
+        break;
 
-                default:
-                case 'h':
-                    display_help(argv[0]);
-                    exit(0);
-                    break;
-                    }
-            }
+      default:
+      case 'h':
+        display_help(argv[0]);
+        exit(0);
+        break;
+    }
+  }
 
-        print_opts();
-        return 0;
+  print_opts();
+  return 0;
 }
