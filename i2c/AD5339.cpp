556c2c7a0be4243cc891878c63d567fefadbffe0
Nuno Barros <barros@lip.pt>
2024-05-24 10:33:55 +0200

    Removed printouts from the DAC class

--- i2c/AD5339.cpp
+++ i2c/AD5339.cpp
@@ -58,24 +58,34 @@ namespace cib
           ptr.dac_b = 1;
           break;
         default:
+#ifdef I2C_PRINT
           SPDLOG_LOGGER_ERROR(m_log,"Unknown channel {}",static_cast<int>(ch));
+#endif
           return CIB_I2C_ErrorInvalidArgument;
       }
       uint16_t word = 0x0;
+#ifdef I2C_PRINT
       SPDLOG_LOGGER_DEBUG(m_log,"Getting level with [{0:x}]",ptr.get_u8());
+#endif
       ret = read_word_register_smbus(ptr.get_u8(),word);
+#ifdef I2C_PRINT
       SPDLOG_LOGGER_TRACE(m_log,"Got ret {0}",ret);
+#endif
       if (ret != CIB_I2C_OK)
       {
         // something failed
         value = 0x0;
         return ret;
       }
+#ifdef I2C_PRINT
 
       SPDLOG_LOGGER_TRACE(m_log,"Received word {0} [0x{0:x}]",word,word);
+#endif
       dac_msg_t msg = *reinterpret_cast<dac_msg_t*>(&word);
       //SPDLOG_LOGGER_TRACE(m_log,"Recasted word {0} [0x{0:x}]",msg.get_u16(),msg.get_u16());
+#ifdef I2C_PRINT
       SPDLOG_LOGGER_DEBUG(m_log,"DAC value : {0} [0x{0:x}]",msg.get_level(),msg.get_level());
+#endif
       value = msg.get_level();
 
       return CIB_I2C_OK;
@@ -102,21 +112,30 @@ namespace cib
           ptr.dac_b = 1;
           break;
         default:
+#ifdef I2C_PRINT
           SPDLOG_LOGGER_ERROR(m_log,"Unknown channel {}",static_cast<int>(ch));
+#endif
           return CIB_I2C_ErrorInvalidArgument;
       }
       dac_msg_t msg;
       msg.clr_bar = 1;
       msg.set_level(value);
+#ifdef I2C_PRINT
       SPDLOG_LOGGER_TRACE(m_log,"Calling write with args [0x{0:x}] [0x{1:x}] (crosscheck ch 0x{2:x} val 0x{3:x})",ptr.get_u8(),msg.get_u16(),static_cast<int>(ch),value);
+#endif
       //printf("About to write 0x%X 0x%X (crosscheck : 0x%X 0x%X)\n",ptr.get_u8(),msg.get_u16(),static_cast<int>(ch),value);
       ret = write_word_register_smbus(ptr.get_u8(),msg.get_u16());
       if (ret != CIB_I2C_OK)
       {
+#ifdef I2C_PRINT
         SPDLOG_LOGGER_ERROR(m_log,"Write reported failure");
+#endif
         return ret;
       }
+#ifdef I2C_PRINT
+
       SPDLOG_LOGGER_TRACE(m_log,"Write complete");
+#endif
       return CIB_I2C_OK;
     }
 
@@ -147,7 +166,9 @@ namespace cib
           ptr.dac_b = 1;
           break;
         default:
+#ifdef I2C_PRINT
           SPDLOG_LOGGER_ERROR(m_log,"Unknown channel {}",static_cast<int>(ch));
+#endif
           return CIB_I2C_ErrorInvalidArgument;
       }
       dac_msg_t msg;
@@ -155,7 +176,9 @@ namespace cib
       ret = write_word_register_smbus(ptr.get_u8(),msg.get_u16());
       if (ret != CIB_I2C_OK)
       {
+#ifdef I2C_PRINT
         SPDLOG_LOGGER_ERROR(m_log,"Write reported failure");
+#endif
         return ret;
       }
       return CIB_I2C_OK;
